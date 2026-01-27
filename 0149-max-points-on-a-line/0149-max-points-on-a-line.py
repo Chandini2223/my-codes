b50998e112ea7602781fa0960ddaa2e791e30018
Chandini2223 <chandinimohammad54@gmail.com>
2026-01-27 20:16:23 +0530

    Time: 43 ms (81.82%), Space: 12.6 MB (48.55%) - LeetHub

---
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.py
@@ -0,0 +1,36 @@
+class Solution:
+    def maxPoints(self, points):
+        if len(points) <= 2:
+            return len(points)
+
+        def gcd(a, b):
+            while b:
+                a, b = b, a % b
+            return a
+
+        ans = 1
+
+        for i in range(len(points)):
+            slopes = {}
+            same = 0
+            curr_max = 0
+            x1, y1 = points[i]
+
+            for j in range(i + 1, len(points)):
+                x2, y2 = points[j]
+
+                dx = x2 - x1
+                dy = y2 - y1
+
+                if dx == 0 and dy == 0:
+                    same += 1
+                else:
+                    g = gcd(dx, dy)
+                    dx //= g
+                    dy //= g
+                    slopes[(dx, dy)] = slopes.get((dx, dy), 0) + 1
+                    curr_max = max(curr_max, slopes[(dx, dy)])
+
+            ans = max(ans, curr_max + same + 1)
+
+        return ans
